5acc6451caf00f847ce5b37305e90a9a2dedcab7
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-08 11:46:44 +0530

    Create Min.java

---
+++ pgm/Min.java
@@ -0,0 +1,35 @@
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Min
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[10];
+		int n,i=0;
+		n=sc.nextInt();
+		for( i=0;i<n;i++)
+		{
+			a[i]=a[i]+1;
+			a[i]=sc.nextInt();
+				System.out.print(" "+a[i]);
+		}
+		int max=a[0];
+		for(i=0;i<n;i++)
+		{
+			if(max<a[i])
+			{
+				max=a[i];
+			}
+		}
+			System.out.println("  "+max);
+		
+	}
+}
+
