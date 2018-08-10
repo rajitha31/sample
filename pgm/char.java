adc9110ff2b11e36dab64432ddfbd37a0e8dfb38
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-10 12:07:50 +0530

    Create char.java

---
+++ pgm/char.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class char
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0,i=0;
+		for(i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+			
+		}
+		System.out.println(""+count);
+	}
+
+}
+	
