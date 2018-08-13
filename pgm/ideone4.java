461bf25e2cbf187e1f55fa3a11d70ab05f6bb80f
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-13 09:42:19 +0530

    Create ideone4.java

---
+++ pgm/ideone4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class ideone4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		int result=a.compareTo(b);
+		if(result==0)
+		System.out.println("both are equal");
+		else if(result>0)
+		System.out.println(a);
+		else
+		System.out.println(b);
+	}
+}
