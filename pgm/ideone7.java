e76c8d84e419c9261d51c0ff773352ccf619d012
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-13 10:08:20 +0530

    Create ideone7.java

---
+++ pgm/ideone7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>0&&n<10)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
