c4d8b5ed82c0f5b1e4a88049b7d45e20d145df51
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-13 09:50:38 +0530

    Create ideone5.java

---
+++ pgm/ideone5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone5
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		String c=a+b;
+		System.out.println(c);
+	}
+}
