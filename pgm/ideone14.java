530764fa80189417ed57d59ea489716eee54fef5
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-13 12:17:27 +0530

    Create ideone14.java

---
+++ pgm/ideone14.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone14
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n==1)
+		System.out.println("one");
+		else if(n==2)
+		System.out.println("two");
+		else if(n==3)
+		System.out.println("three");
+		else if(n==4)
+		System.out.println("four");
+		else if(n==5)
+		System.out.println("five");
+		else if(n==6)
+		System.out.println("six");
+		else if(n==7)
+		System.out.println("seven");
+		else if(n==8)
+		System.out.println("eight");
+		else if(n==9)
+		System.out.println("nine");
+		else if(n==10)
+		System.out.println("ten");
+		else
+		System.out.println("please enter between 1.10 only");
+	}
+}
