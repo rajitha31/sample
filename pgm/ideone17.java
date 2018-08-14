9daf1c09f03eecbc7cbe095d2457d4ce534593ee
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-14 11:14:14 +0530

    Create ideone17.java

---
+++ pgm/ideone17.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone17
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,m,d;
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		d=m*n;
+		if(d%2==0)
+		System.out.println("even");
+		else
+		System.out.println("odd");
+	}
+}
