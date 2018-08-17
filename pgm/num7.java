fc3b0d9b79c8bfc0d56125c2c77a21aba00e6f43
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-17 09:56:48 +0530

    Create num7.java

---
+++ pgm/num7.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class num7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int l=sc.nextInt();
+		int r=sc.nextInt();
+		int i;
+		for(i=l;i<=r;i++)
+		{
+			if(i==l)
+			{
+				System.out.println("no");
+				break;
+			}
+			else if(i==n)
+				System.out.println("yes");
+			else
+			{
+ 
+			}
+		}
+		if(i==r)
+			System.out.println("no");
+	}
+}
