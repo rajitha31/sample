6d88e79866c7a7d202c979602cc9aaf8a1b200e7
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-17 09:28:19 +0530

    Update and rename ideone36.java to num5.java

---
+++ pgm/num5.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class num5
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+
+	Scanner in=new Scanner(System.in);
+        String s=in.nextLine();
+        StringBuffer sb=new StringBuffer(s);
+        sb.reverse();
+        if(sb.toString().equals(s))
+        {
+        	System.out.print("yes");
+        }
+        else
+        	System.out.print("no");
+	}
+}
+
+		
