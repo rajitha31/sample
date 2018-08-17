df8c01d9eef483632e0cade110d72b7d8b3851aa
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-17 10:01:50 +0530

    Create num8.java

---
+++ pgm/num8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class num8
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float d;
+        Scanner s=new Scanner(System.in);
+        d=s.nextFloat();
+      int v=  Math.round(d);
+      System.out.println(v);
+ 
+	}
+}
