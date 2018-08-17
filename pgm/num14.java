ccd76d19290fd9ab99ea05db338ea9625b359823
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-17 12:05:06 +0530

    Create num14.java

---
+++ pgm/num14.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class num14
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i=0;
+		int arr[]=new int[20];
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		while(n!=0)
+		{
+			int r=n%10;
+			if(r%2!=0)
+			{
+				arr[i++]=r;
+			}
+			n=n/10;
+		}
+		for(int j=i-1;j>=0;j--)
+		{
+			System.out.print(arr[j]+" ");
+		}
+	}
+}
