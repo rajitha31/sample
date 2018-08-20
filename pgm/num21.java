8bddc45bec8c4a574f23d744b245a6803e91103f
rajitha31 <41733657+rajitha31@users.noreply.github.com>
2018-08-20 10:28:21 +0530

    Create num21.java

---
+++ pgm/num21.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class num21
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+       Scanner s=new Scanner(System.in);
+       float a=s.nextFloat();
+       float b=s.nextFloat();
+       float c=a*b;
+       System.out.printf("%f",c);
+	}
+}
